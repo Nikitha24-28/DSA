a197531cfc674ea2bcb5dd09a863b73561e6b86c
Nikitha G <nikichowdary976@gmail.com>
2026-09-15 23:07:11 +0530

    Time: 4 ms (50.79%) | Memory: 17 MB (56.98%) - LeetSync

---
+++ 179-largest-number/largest-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    static bool cmp(string a ,string b){
+        string p=a+b;
+        string q=b+a;
+        return p>q;
+    } 
+    string largestNumber(vector<int>& nums) {
+        vector<string> arr;
+        for(int x:nums){
+            arr.push_back(to_string(x));
+        }
+        sort(arr.begin(),arr.end(),cmp);
+        if(arr[0]=="0") return "0";
+        string ans="";
+        for(string x:arr){
+            ans+=x;
+        }
+        return ans;
+    }
+};
