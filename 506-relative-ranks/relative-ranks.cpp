c894eca6cea1b2883b5c0edcb762e8602734b8af
Nikitha G <nikichowdary976@gmail.com>
2026-09-18 22:43:01 +0530

    Time: 3 ms (79.71%) | Memory: 15.5 MB (77.20%) - LeetSync

---
+++ 506-relative-ranks/relative-ranks.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        priority_queue<pair<int,int>> pq;
+        for(int i=0;i<score.size();i++){
+            pq.push({score[i],i});
+        }
+        vector<string> ans(score.size());
+        int rank=1;
+        while(!pq.empty()){
+            auto ele=pq.top();
+            int sco=ele.first;
+            int idx=ele.second;
+            pq.pop();
+            if(rank==1){
+                ans[idx]="Gold Medal";
+            }
+            else if(rank==2){
+                ans[idx]="Silver Medal";
+            }
+            else if(rank==3){
+                ans[idx]="Bronze Medal";
+            }
+            else{
+                ans[idx]=to_string(rank);
+            }
+            rank++;
+            
+        }
+        return ans;
+    }
+};
